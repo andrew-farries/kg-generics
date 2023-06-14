75f02f64816192f518f9846c902ccfefb585aa2d
Andrew Farries <andrew.farries@xata.io>
2023-06-14 20:47:35 +0100

    More chapter 5

--- exercises/5.1/sequence.go
+++ exercises/5.1/sequence.go
@@ -5,3 +5,33 @@ type Sequence[E any] []E
 func (s Sequence[E]) Empty() bool {
 	return len(s) == 0
 }
+
+func (s Sequence[E]) First() E {
+	var fst E
+
+	if len(s) > 0 {
+		fst = s[0]
+	}
+	return fst
+}
+
+// We can't have type parameters on methods :(
+// func (s Sequence[E]) PrintWith[T any](x T) {
+//   fmt.Println(x, s)
+// }
+
+// Maps
+
+type Catalog[V any] map[string]V
+
+type Index[K comparable, V any] map[K]V
+
+// Structs
+
+type NamedThing[T any] struct {
+	Thing T
+	Name  string
+}
+
+// Channels
+type MyChan[E any] chan E
--- exercises/5.1/sequence_test.go
+++ exercises/5.1/sequence_test.go
@@ -1,6 +1,8 @@
 package sequence_test
 
 import (
+	"errors"
+	"fmt"
 	"sequence"
 	"testing"
 )
@@ -21,4 +23,46 @@ func TestEmptyIsFalseForNonEmptySequence(t *testing.T) {
 	if got {
 		t.Fatal("true for non-empty sequence")
 	}
+}
+
+// Generic types can be instantiated with interface types
+func TestInstantiation(t *testing.T) {
+	x := sequence.Sequence[error]{errors.New("oh no")}
+
+	fmt.Println(x)
+}
+
+func TestMaps(t *testing.T) {
+	m := sequence.Catalog[int]{}
+
+	fmt.Println(m["bob"])
+
+	m2 := sequence.Index[string, int]{}
+	m2["bob"] = 31
+	fmt.Println(m2["bob"])
+}
+
+func TestStructs(t *testing.T) {
+	x := sequence.NamedThing[int]{Thing: 2, Name: "bob"}
+	y := sequence.NamedThing[sequence.NamedThing[int]]{
+		Thing: sequence.NamedThing[int]{
+			Thing: 2,
+			Name:  "Alice",
+		},
+		Name: "bob",
+	}
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+func TestChannels(t *testing.T) {
+	ch := make(sequence.MyChan[string])
+
+	go func() {
+		defer close(ch)
+		ch <- "hello world"
+	}()
+
+	fmt.Println(<-ch)
 }
