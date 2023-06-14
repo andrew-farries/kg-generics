385678672fc7e9279f7565a92a034bb902ede61a
Andrew Farries <andrew.farries@xata.io>
2023-06-14 20:52:31 +0100

    Solve 6.1

--- exercises/6.1/funcmap.go
+++ exercises/6.1/funcmap.go
@@ -1,3 +1,12 @@
 package funcmap
 
-// Your implementation of FuncMap goes here!
+type FuncMap[T, U any] map[string]func(T) U
+
+func (m FuncMap[T, U]) Apply(f string, x T) U {
+	fn, ok := m[f]
+	if !ok {
+		panic("function not found")
+	}
+
+	return fn(x)
+}
