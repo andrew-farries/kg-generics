0a134e1bf6267910a610b4116a6413e29210d908
Andrew Farries <andrew.farries@xata.io>
2023-06-14 17:12:51 +0100

    Solution for 3.1

--- exercises/3.1/stringy.go
+++ exercises/3.1/stringy.go
@@ -1,3 +1,12 @@
 package stringy
 
-// Your StringifyTo function goes here!
+import (
+	"fmt"
+	"io"
+)
+
+// A useless use of generics
+// (it doesn't relate two or more types in the signature)
+func StringifyTo[T fmt.Stringer](w io.Writer, x T) {
+	fmt.Fprintln(w, x)
+}
