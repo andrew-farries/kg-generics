e28102b59d5db45dce9089179ad5a3914f2b830e
Andrew Farries <andrew.farries@xata.io>
2023-06-14 22:30:11 +0100

    Solve 7.1

--- exercises/7.1/stack.go
+++ exercises/7.1/stack.go
@@ -1,3 +1,23 @@
 package stack
 
-// Your implementation of Stack goes here!
+type Stack[E any] struct {
+	xs []E
+}
+
+func (s *Stack[E]) Push(xs ...E) {
+	s.xs = append(s.xs, xs...)
+}
+
+func (s *Stack[E]) Pop() (v E, ok bool) {
+	if s.Len() == 0 {
+		return v, false
+	}
+	v = s.xs[len(s.xs)-1]
+	s.xs = s.xs[:len(s.xs)-1]
+	return v, true
+}
+
+func (s *Stack[E]) Len() int {
+	return len(s.xs)
+}
+
