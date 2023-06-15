e2d62d2b3cc887dfbb1807a609f8b23ee14b9c65
Andrew Farries <andrew.farries@xata.io>
2023-06-15 12:20:11 +0100

    Solve 9.2

--- exercises/9.2/merge.go
+++ exercises/9.2/merge.go
@@ -2,4 +2,11 @@ package merge
 
 import "golang.org/x/exp/maps"
 
-// Your implementation of Merge goes here!
+func Merge[K comparable, T any](ms ...map[K]T) map[K]T {
+	dst := map[K]T{}
+
+	for _, m := range ms {
+		maps.Copy(dst, m)
+	}
+	return dst
+}
