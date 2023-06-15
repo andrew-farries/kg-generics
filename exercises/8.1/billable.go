e87769b238c214e81c320984493de84833802184
Andrew Farries <andrew.farries@xata.io>
2023-06-15 11:20:02 +0100

    Solve 8.1

--- exercises/8.1/billable.go
+++ exercises/8.1/billable.go
@@ -1,3 +1,50 @@
 package billable
 
-// Your implementation of Channel goes here!!
+import (
+	"sync"
+)
+
+type Channel[T any] struct {
+	ch         chan T
+	sendsMu    sync.RWMutex
+	sends      int
+	receivesMu sync.RWMutex
+	receives   int
+}
+
+func NewChannel[T any](capacity int) Channel[T] {
+	return Channel[T]{
+		ch: make(chan T, capacity),
+	}
+}
+
+func (c *Channel[T]) Send(x T) {
+	c.ch <- x
+
+	c.sendsMu.Lock()
+	defer c.sendsMu.Unlock()
+	c.sends += 1
+}
+
+func (c *Channel[T]) Receive() T {
+	x := <-c.ch
+
+	c.receivesMu.Lock()
+	defer c.receivesMu.Unlock()
+	c.receives += 1
+	return x
+}
+
+func (c *Channel[T]) Sends() int {
+	c.sendsMu.RLock()
+	defer c.sendsMu.RUnlock()
+
+	return c.sends
+}
+
+func (c *Channel[T]) Receives() int {
+	c.receivesMu.RLock()
+	defer c.receivesMu.RUnlock()
+
+	return c.receives
+}
